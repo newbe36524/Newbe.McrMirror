b875ad498de85655ebcd4540196432a2a9c7ff33
newbe36524 <newbe36524@qq.com>
2020-06-10 09:42:28 +0800

    init

---
+++ src/create.ps1
@@ -0,0 +1,12 @@
+$config = Get-Content config.json | ConvertFrom-Json
+$tpl = Get-Content .\template.yml
+
+$outDir = "../.github/workflows"
+
+$config.images | ForEach-Object {
+    $sourceName = $_[0]
+    $targetName = $_[1]
+    $filename = $targetName.Replace("/","_").Replace(":","_")
+    $content = $tpl.Replace("[FILE_NAME]", $filename).Replace("[SOURCE_IMAGE_NAME]", $sourceName).Replace("[TARGET_IMAGE_NAME]", $targetName).Replace("[DOCKERHUB_USERNAME]", $config.dockerhub_name)
+    $content | Out-File "$outDir/$($filename).yml"
+}
