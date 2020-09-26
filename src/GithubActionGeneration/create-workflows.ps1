80f6969e56e00aa1c2376c7f969e8c103313bc63
newbe36524 <newbe36524@qq.com>
2020-09-26 16:06:50 +0800

    refact flow with matrix

--- src/GithubActionGeneration/create-workflows.ps1
+++ src/GithubActionGeneration/create-workflows.ps1
@@ -1,12 +1,30 @@
 $config = Get-Content config-v2.json | ConvertFrom-Json
-$tpl = Get-Content .\template.yml
 
 $outDir = "../../.github/workflows"
 
+# $config.images | ForEach-Object {
+#     $sourceName = $_.source
+#     $targetName = $_.tag
+#     $filename = $targetName.Replace("/", "_").Replace(":", "_")
+#     $content = $tpl.Replace("[FILE_NAME]", $filename).Replace("[SOURCE_IMAGE_NAME]", $sourceName).Replace("[TARGET_IMAGE_NAME]", $targetName).Replace("[DOCKERHUB_USERNAME]", $config.dockerhub_name).Replace("[DOCKERHUB_NAMESPACE]", $config.dockerhub_namespace).Replace("[ALIYUN_USERNAME]", $config.aliyun_name).Replace("[ALIYUN_NAMESPACE]", $config.aliyun_namespace).Replace("[TENCENTYUN_USERNAME]", $config.tencentyun_name).Replace("[TENCENTYUN_NAMESPACE]", $config.tencentyun_namespace)
+#     $content | Out-File "$outDir/docker_$($filename).yml" -Encoding utf8NoBOM
+# }
+
+$images = ""
 $config.images | ForEach-Object {
     $sourceName = $_.source
     $targetName = $_.tag
-    $filename = $targetName.Replace("/", "_").Replace(":", "_")
-    $content = $tpl.Replace("[FILE_NAME]", $filename).Replace("[SOURCE_IMAGE_NAME]", $sourceName).Replace("[TARGET_IMAGE_NAME]", $targetName).Replace("[DOCKERHUB_USERNAME]", $config.dockerhub_name).Replace("[DOCKERHUB_NAMESPACE]", $config.dockerhub_namespace).Replace("[ALIYUN_USERNAME]", $config.aliyun_name).Replace("[ALIYUN_NAMESPACE]", $config.aliyun_namespace).Replace("[TENCENTYUN_USERNAME]", $config.tencentyun_name).Replace("[TENCENTYUN_NAMESPACE]", $config.tencentyun_namespace)
+    $images += "          - $sourceName,$targetName`n"
+}
+$templates = @(
+    "template_aliyun.yml",
+    "template_dockerhub.yml",
+    "template_tencent.yml"
+)
+$templates | ForEach-Object {
+    $server = "$_".Replace("template_","").Replace(".yml","")
+    $filename = "dotnet_$server"
+    $tpl = Get-Content .\$_
+    $content = $tpl.Replace("[FILE_NAME]", $filename).Replace("[SOURCE_IMAGE_NAME]", $sourceName).Replace("[TARGET_IMAGE_NAME]", $targetName).Replace("[DOCKERHUB_USERNAME]", $config.dockerhub_name).Replace("[DOCKERHUB_NAMESPACE]", $config.dockerhub_namespace).Replace("[ALIYUN_USERNAME]", $config.aliyun_name).Replace("[ALIYUN_NAMESPACE]", $config.aliyun_namespace).Replace("[TENCENTYUN_USERNAME]", $config.tencentyun_name).Replace("[TENCENTYUN_NAMESPACE]", $config.tencentyun_namespace).Replace("[ALL_IMAGE]", $images)
     $content | Out-File "$outDir/docker_$($filename).yml" -Encoding utf8NoBOM
 }
