5e62dc1f8a0494164329d59b135b0ec756dfc3b3
newbe36524 <newbe36524@qq.com>
2020-09-26 18:00:31 +0800

    fix

--- src/GithubActionGeneration/tasks.py
+++ src/GithubActionGeneration/tasks.py
@@ -8,12 +8,18 @@ def info(c):
 
 
 @task
+def pull_image(c, data):
+    data.split(',')
+    source_image_name = data[0]
+    c.run(f"docker pull {source_image_name}")
+
+
+@task
 def docker_sync_dockerhub(c, data):
     data.split(',')
     source_image_name = data[0]
     target_image_name = data[1]
     namespace = os.environ['DOCKERHUB_NAMESPACE']
-    c.run(f"docker pull {source_image_name}")
     c.run(f"docker tag {source_image_name} {namespace}/{target_image_name}")
     c.run(f"docker push {namespace}/{target_image_name}")
 
@@ -24,7 +30,6 @@ def docker_sync_aliyun(c, data):
     source_image_name = data[0]
     target_image_name = data[1]
     namespace = os.environ['ALIYUN_NAMESPACE']
-    c.run(f"docker pull {source_image_name}")
     c.run(f"docker tag {source_image_name} registry.cn-hangzhou.aliyuncs.com/{namespace}/{target_image_name}")
     c.run(f"docker push registry.cn-hangzhou.aliyuncs.com/{namespace}/{target_image_name}")
 
@@ -35,6 +40,5 @@ def docker_sync_tencent(c, data):
     source_image_name = data[0]
     target_image_name = data[1]
     namespace = os.environ['TENCENTYUN_NAMESPACE']
-    c.run(f"docker pull {source_image_name}")
     c.run(f"docker tag {source_image_name} ccr.ccs.tencentyun.com/{namespace}/{target_image_name}")
     c.run(f"docker push ccr.ccs.tencentyun.com/{namespace}/{target_image_name}")
