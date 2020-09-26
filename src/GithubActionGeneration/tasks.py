99e2277f4eb47df6c92a7b11bbb4ce6fba2e6d54
Newbe36524 <newbe36524@qq.com>
2020-09-26 18:33:08 +0800

    Refact with pyinvoke (#13)

---
+++ src/GithubActionGeneration/tasks.py
@@ -0,0 +1,46 @@
+from invoke import task
+import os
+
+
+@task
+def info(c):
+    c.run("echo info")
+
+
+@task
+def pull_image(c, data):
+    strings = data.split(',')
+    source_image_name = strings[0]
+    cmd = f"docker pull {source_image_name}"
+    print(cmd)
+    c.run(cmd)
+
+
+@task
+def docker_sync_dockerhub(c, data):
+    strings = data.split(',')
+    source_image_name = strings[0]
+    target_image_name = strings[1]
+    namespace = os.environ['DOCKERHUB_NAMESPACE']
+    c.run(f"docker tag {source_image_name} {namespace}/{target_image_name}")
+    c.run(f"docker push {namespace}/{target_image_name}")
+
+
+@task
+def docker_sync_aliyun(c, data):
+    strings = data.split(',')
+    source_image_name = strings[0]
+    target_image_name = strings[1]
+    namespace = os.environ['ALIYUN_NAMESPACE']
+    c.run(f"docker tag {source_image_name} registry.cn-hangzhou.aliyuncs.com/{namespace}/{target_image_name}")
+    c.run(f"docker push registry.cn-hangzhou.aliyuncs.com/{namespace}/{target_image_name}")
+
+
+@task
+def docker_sync_tencent(c, data):
+    strings = data.split(',')
+    source_image_name = strings[0]
+    target_image_name = strings[1]
+    namespace = os.environ['TENCENTYUN_NAMESPACE']
+    c.run(f"docker tag {source_image_name} ccr.ccs.tencentyun.com/{namespace}/{target_image_name}")
+    c.run(f"docker push ccr.ccs.tencentyun.com/{namespace}/{target_image_name}")
